3a2ddeb1bd6cf3b8b6c23d0ad248a53d95e0fa72
ximing <ximing@meituan.com>
2022-04-03 16:40:00 +0800

    加上 ts、构建和测试配置

---
+++ jest.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  preset: 'ts-jest',
+  setupFilesAfterEnv: ['./scripts/testSetup.js'],
+  testRegex: '\\.test\\.tsx?$',
+  collectCoverage: false,
+  testEnvironment: 'node',
+  coverageReporters: ['lcovonly', 'text'],
+  collectCoverageFrom: ['src/**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  coveragePathIgnorePatterns: ['node_modules', '<rootDir>/src/index.ts', '.mock.ts'],
+  // moduleNameMapper: {
+  //   'react-platform': 'react-dom',
+  // },
+};
