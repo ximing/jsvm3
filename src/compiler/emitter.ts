3479bc1a72554583322a43dae027d052fd1b1d75
ximing <ximing@meituan.com>
2022-05-08 22:41:00 +0800

    emitter 支持字面量和标识符

---
+++ src/compiler/emitter.ts
@@ -0,0 +1,304 @@
+import { Visitor } from './visitor';
+import { hasProp } from '../utils/helper';
+import { Instruction } from '../opcodes/types';
+import {
+  COLUMN,
+  GETG,
+  GETL,
+  LINE,
+  LITERAL,
+  STRING_LITERAL,
+  UNDEF,
+} from '../opcodes';
+import { Label } from '../opcodes/label';
+import { Script } from '../vm/script';
+import { Guard } from '../vm/types';
+
+type EmitterLabel = {
+  name: string | null;
+  stmt: any;
+  brk?: Label;
+  cont?: Label;
+  cleanup?: any[] | null;
+};
+
+export class Emitter extends Visitor {
+  fName: string;
+  name: string | null;
+  original: string[];
+  source: string;
+  instructions: Instruction[];
+  labels: EmitterLabel[];
+  children: any[];
+  tryStatements: any[];
+  withLevel: number;
+  scopes: any[];
+  scriptScope: any;
+  globalNames: any[];
+  localNames: any[];
+  varIndex: number;
+  guards: Guard[];
+  currentLine: number;
+  currentColumn: number;
+  stringIds: Map<string, number>;
+  strings: string[];
+  regexpIds: Map<string, number>;
+  regexps: RegExp[];
+  ignoreNotDefined: number;
+
+  constructor(
+    scopes: any[] | null,
+    fName: string,
+    name: string | null,
+    original: string[],
+    source: string
+  ) {
+    super();
+    this.fName = fName;
+    this.name = name;
+    this.original = original;
+    this.source = source;
+    this.instructions = [];
+    this.labels = [];
+    this.children = [];
+    this.tryStatements = [];
+    this.withLevel = 0;
+    // Stack of scopes. Each scope maintains a name -> index association
+    // where index is unique per script(function or code executing in global scope)
+    this.scopes = scopes || [];
+    if (scopes) {
+      this.scriptScope = scopes[0];
+    }
+    this.globalNames = [];
+    this.localNames = [];
+    this.varIndex = 3;
+    this.guards = [];
+    this.currentLine = -1;
+    this.currentColumn = -1;
+    this.stringIds = new Map();
+    this.strings = [];
+    this.regexpIds = new Map();
+    this.regexps = [];
+    this.ignoreNotDefined = 0;
+  }
+
+  createINS(op: (args: any[] | null) => Instruction, ...args) {
+    if (!op) {
+      throw new Error('op必传');
+    }
+    if (!args.length) {
+      // @ts-ignore
+      args = null;
+    }
+    this.instructions.push(op(args));
+  }
+
+  scope(name: string) {
+    let i = 0;
+    let crossFunctionScope = false;
+    for (const scope of this.scopes) {
+      if (hasProp(scope, name)) {
+        return [i, scope[name]];
+      }
+      // 只有在函数scope之后的scopes才会增加索引
+      if (crossFunctionScope || scope === this.scriptScope) {
+        crossFunctionScope = true;
+        i++;
+      }
+    }
+    return null;
+  }
+
+  scopeGet(name: string) {
+    const scope = this.scope(name);
+    if (scope) {
+      this.ignoreNotDefined = 0;
+      this.createINS(GETL, ...scope);
+      return;
+    }
+    const idx = this.globalIdx(name);
+    this.createINS(GETG, idx, this.ignoreNotDefined);
+    this.ignoreNotDefined = 0;
+  }
+
+  globalIdx(name: string) {
+    for (let i = 0; i < this.globalNames.length; i++) {
+      if (this.globalNames[i] === name) {
+        return i;
+      }
+    }
+    return this.globalNames.push(name) - 1;
+  }
+
+  newLabel() {
+    return new Label(this);
+  }
+
+  label(name?: string | null) {
+    if (!name) {
+      return this.labels[this.labels.length - 1];
+    }
+    for (const label of this.labels) {
+      // console.log('---->', label.name, name);
+      if (label.name === name) {
+        return label;
+      }
+    }
+    return null;
+  }
+
+  pushLabel(name: string | null, stmt: any, brk?: Label, cont?: Label) {
+    // console.log('pushLabel', name, stmt, 'brk', brk, 'cont', cont);
+    return this.labels.push({ name, stmt, brk, cont });
+  }
+
+  popLabel() {
+    return this.labels.pop();
+  }
+
+  end() {
+    let code, max;
+    // console.log('this.instructions', this.instructions);
+    for (code of Array.from(this.instructions)) {
+      code.forEachLabel(function (l) {
+        if (l.ip === null) {
+          throw new Error('label has not been marked');
+        }
+        return l.ip;
+      });
+    }
+    for (const guard of Array.from(this.guards)) {
+      guard.start = (guard.start as Label).ip;
+      if (guard.handler) {
+        guard.handler = (guard.handler as Label).ip;
+      }
+      if (guard.finalizer) {
+        guard.finalizer = (guard.finalizer as Label).ip;
+      }
+      guard.end = (guard.end as Label).ip;
+    }
+    // calculate the maximum evaluation stack size
+    // at least 2 stack size is needed for the arguments object
+    // and the self function reference
+    let current = (max = 2);
+    for (code of this.instructions) {
+      current += code.calculateFactor();
+      max = Math.max(current, max);
+    }
+    const localLength = this.localNames.length;
+    // compile all functions
+    for (let i = 0, end = this.children.length; i < end; i++) {
+      this.children[i] = this.children[i]();
+    }
+    return new Script(
+      this.fName,
+      this.name,
+      this.instructions,
+      this.children,
+      this.localNames,
+      localLength,
+      this.globalNames,
+      this.guards,
+      max,
+      this.strings,
+      this.regexps,
+      this.source
+    );
+  }
+
+  visit(node: any) {
+    if (node == null) {
+      // eg: the 'alternate' block of an if statement
+      return;
+    }
+    if (node.loc) {
+      let idx;
+      const { line, column } = node.loc.start;
+      /*
+       * 首先检查节点的起始行号（node.loc.start.line）
+       * 如果这个行号与this.currentLine（当前正在处理的行号）不同，表示代码进入了一个新的行。
+       * 此时，会移除指令列表中末尾的与行号或列号相关的指令（这是为了避免重复或错误地标记行号），然后生成一个新的LINE指令来标记这个新行的开始，并更新this.currentLine。
+       * 如果节点的起始列号（node.loc.start.column）与this.currentColumn不同，
+       * 同样会移除指令列表中末尾的与列号相关的指令，生成一个新的COLUMN指令来标记新列的开始，并更新this.currentColumn。
+       * */
+      if (line !== this.currentLine) {
+        idx = this.instructions.length - 1;
+        while (
+          idx >= 0 &&
+          (this.instructions[idx].name === 'LINE' || this.instructions[idx].name === 'COLUMN')
+        ) {
+          this.instructions.pop();
+          idx--;
+        }
+        this.createINS(LINE, line);
+        this.currentLine = line;
+      } else if (column !== this.currentColumn) {
+        idx = this.instructions.length - 1;
+        while (idx >= 0 && this.instructions[idx].name === 'COLUMN') {
+          this.instructions.pop();
+          idx--;
+        }
+        this.createINS(COLUMN, column);
+        this.currentColumn = column;
+      }
+    }
+    return super.visit(node);
+  }
+
+  Identifier(node) {
+    // 一个标识符。请注意，标识符可以是表达式(expression)或解构模式(destructuring pattern)
+    this.scopeGet(node.name);
+    return node;
+  }
+
+  StringLiteral(node: t.StringLiteral) {
+    this.Literal(node);
+    return node;
+  }
+
+  NumericLiteral(node: t.NumericLiteral) {
+    this.Literal(node);
+    return node;
+  }
+
+  BooleanLiteral(node: t.BooleanLiteral) {
+    this.Literal(node);
+    return node;
+  }
+
+  NullLiteral(node: t.NullLiteral) {
+    this.createINS(LITERAL, null);
+    return node;
+  }
+
+  Literal(node: t.Literal) {
+    // @ts-ignore
+    this.createLiteral(node.value);
+    return node;
+  }
+
+  createString(val: string) {
+    if (!this.stringIds.has(val)) {
+      this.strings.push(val);
+      const idx = this.strings.length - 1;
+      this.stringIds.set(val, idx);
+      return idx;
+    }
+    return this.stringIds.get(val);
+  }
+
+  createLiteral(val: any) {
+    let idx;
+    if (typeof val === 'undefined') {
+      this.createINS(UNDEF);
+      // variable-length literals(strings and regexps) are stored in arrays
+      // and referenced by index
+    } else if (typeof val === 'string') {
+      idx = this.createString(val);
+      this.createINS(STRING_LITERAL, idx);
+    } else {
+      this.createINS(LITERAL, val);
+    }
+  }
+
+}
