c6b2625816502ff1f06c725c68e8be56c06fcf2b
ximing <ximing@meituan.com>
2022-05-22 22:15:00 +0800

    补几个简单测试

---
+++ __tests__/helper.ts
@@ -0,0 +1,21 @@
+import { transformEXP, transform } from '../src/compiler';
+import { JSVM } from '../src/vm/vm';
+
+export const run = function (code, ctx = {}, hoisting = true, convertES5 = false) {
+  const script = transform(code, 'test.js', { hoisting, convertES5 });
+  const vm = new JSVM(Object.assign({ Map: Map, WeakMap: WeakMap, Set: Set, Proxy: Proxy }, ctx));
+  // console.log(JSON.stringify(script.toJSON(), null, 2));
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const res = vm.exec(script);
+  // console.log(res);
+  // console.log(JSON.stringify(script.toJSON(), null, 2));
+  // console.log(vm.realm.globalObj);
+  return (vm.realm.globalObj as any).module.exports;
+};
+
+export const runExp = function (code: string, ctx = {}) {
+  const script = transformEXP(code);
+  const vm = new JSVM(ctx);
+  const res = vm.exec(script);
+  return res;
+};
