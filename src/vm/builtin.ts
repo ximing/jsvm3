7f4e09c5b18b3e0fcfd1bf2c02171a018f85d304
ximing <ximing@meituan.com>
2022-08-21 22:29:00 +0800

    补全内建对象

--- src/vm/builtin.ts
+++ src/vm/builtin.ts
@@ -10,3 +10,18 @@ export class StopIteration extends JSVMError {
     this.message = message;
   }
 }
+export class ArrayIterator {
+  elements: any[];
+  index: number;
+  constructor(elements) {
+    this.elements = elements;
+    this.index = 0;
+  }
+
+  next() {
+    if (this.index >= this.elements.length) {
+      throw new StopIteration();
+    }
+    return this.elements[this.index++];
+  }
+}
--- src/vm/realm.ts
+++ src/vm/realm.ts
@@ -1,3 +1,15 @@
+import {
+  JSVMError,
+  JSVMTypeError,
+  JSVMRangeError,
+  JSVMReferenceError,
+  JSVMSyntaxError,
+} from '../utils/errors';
+
+// @if CURRENT != 'exp'
+import { StopIteration } from './builtin';
+// @endif
+
 /*
 * 在 JavaScript 中，Realm 可以看作是一个全局环境，它包含了 JavaScript 的内置对象（如 Array、Object、Function 等）以及相关的执行环境。
 * 每个 Realm 都有自己的全局对象，不同的 Realm 之间是完全隔离的。
@@ -8,6 +20,38 @@ export class Realm {
     // @if CURRENT != 'all'
     this.globalObj = {};
     // @endif
+
+    // @if CURRENT == 'all'
+    this.globalObj = {
+      undefined,
+      Object,
+      Function,
+      Number,
+      Boolean,
+      String,
+      Array,
+      Date,
+      RegExp,
+      Error: JSVMError,
+      TypeError: JSVMTypeError,
+      RangeError: JSVMRangeError,
+      ReferenceError: JSVMReferenceError,
+      SyntaxError: JSVMSyntaxError,
+      // EvalError: JSVMEvalError,
+      // URIError: JSVMURIError,
+      StopIteration,
+      Math,
+      JSON,
+      parseInt,
+      parseFloat,
+      console,
+      NaN,
+      Promise,
+      Infinity,
+      isNaN:Number.isNaN.bind(Number)
+    };
+    // @endif
+
     for (const k of Object.keys(merge)) {
       const v = merge[k];
       this.globalObj[k] = v;
