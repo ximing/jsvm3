baa13efae069588b9ddc172ff5d5db79d5a01efd
ximing <ximing@meituan.com>
2022-11-06 22:24:00 +0800

    richards 和 raytrace 跑起来

---
+++ benchmark/jsvm3.js
@@ -0,0 +1,12 @@
+const { transform } = require('../lib/compiler');
+const { JSVM } = require('../lib/vm/vm');
+// const { JSVM } = require('../dist');
+const code = require('fs').readFileSync(require('path').join(__dirname, './benchmark.js'), 'utf-8');
+const script = transform(code, 'sum.js', { hoisting: true, convertES5: false });
+
+const vm1 = new JSVM({
+  console,
+  require,
+});
+
+vm1.exec(script);
